d819d0c5e14d7223d4b6cc482d8717b3efa29962
riya925 <60502810+riya925@users.noreply.github.com>
2024-06-23 19:00:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public void btd()
+        {
+            int a;
+            int i = 0;
+            double rem, result = 0;
+            Console.Write("\nEnter Binary Number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            while (a != 0)
+            {
+                rem = a % 10;
+                result = result + rem * Math.Pow(2, i++);
+                a = a / 10;
+            }
+            Console.WriteLine("Decimal Number is : " + result);
+            Console.ReadLine();
+        }
+        public void Main(string[] args)
+        {
+            Console.WriteLine("\n Riya Shah \t 170410107108 ");
+            Program p = new Program();
+            p.btd();
+        }
+    }
+}
